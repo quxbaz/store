9206711fc967b615c9ff421cebe62675beee1b9e
David Yeung <quxbaz@gmail.com>
2016-01-22 22:12:16 -0500

    Moved test_store to top of imports

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ require('source-map-support').install();
 let should = require('chai').should();
 
 // Tests
+import './test_store';
 import './test_util';
 import './test_bin';
 import './test_server';
