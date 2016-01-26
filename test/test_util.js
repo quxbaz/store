bd1320238eeed715ddd95d8ce4503943e9c7426b
David Yeung <quxbaz@gmail.com>
2016-01-25 19:58:43 -0500

    Added util.cid

--- test/test_util.js
+++ test/test_util.js
@@ -75,4 +75,15 @@ describe("lib/util", () => {
     });
   });
 
+  describe("cid()", () => {
+    it("Creates 1000 unique cids that all begin with 'c'", () => {
+      let cids = new Set();
+      for (let i=0; i < 1000; i++)
+        cids.add(util.cid());
+      cids.size.should.eql(1000);
+      for (let cid of cids)
+        cid[0].should.eql('c');
+    });
+  });
+
 });
