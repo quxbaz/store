c607137659f77a38adff1e787194ea8fc73112cc
David Yeung <quxbaz@gmail.com>
2016-01-25 19:43:40 -0500

    record now strips unpersisted records from save data

--- test/test_model.js
+++ test/test_model.js
@@ -42,6 +42,8 @@ describe("lib/model", () => {
         zoo: belongsTo('zoo')
       });
       server.bin.set('/zoo/1', {id: 1, city: 'chicago'});
+      server.bin.set('/cat/2', {id: 2, zoo: 1, name: 'mittens'});
+      server.bin.set('/cat/3', {id: 3, zoo: 1, name: 'whiskers'});
     });
 
     it("Throws an error on defining a relation that is not a Relation object.", () => {
@@ -53,8 +55,6 @@ describe("lib/model", () => {
     });
 
     it("Gets a record and its hasMany records.", () => {
-      server.bin.set('/cat/2', {id: 2, zoo: 1, name: 'mittens'});
-      server.bin.set('/cat/3', {id: 3, zoo: 1, name: 'whiskers'});
       return store.get('zoo', 1).then((zoo) => {
         return zoo.get('cats');
       }).then((cats) => {
@@ -65,12 +65,44 @@ describe("lib/model", () => {
     });
 
     it("Gets a record's belongsTo records.", () => {
-      server.bin.set('/cat/2', {id: 2, zoo: 1, name: 'mittens'});
       return store.get('cat', 2).then((cat) => {
         return cat.get('zoo');
       }).then((zoo) => {
         zoo.state.city.should.eql('chicago');
       });
+    });
+
+    it("Keeps saved and unsaved hasMany records.", () => {
+      let yowzers = store.createRecord('cat', {zoo: 1, name: 'yowzers'});
+      return store.get('zoo').then(
+        () => yowzers.get('zoo')
+      ).then(
+        zoo => zoo.get('cats')
+      ).then((cats) => {
+        let names = cats.map(cat => cat.state.name);
+        names.sort().should.eql(['mittens', 'whiskers', 'yowzers']);
+      });
+    });
+
+    it("Gets an unsaved belongsTo record.", () => {
+      false.should.eql(true);
+      let zoo = store.createRecord('zoo', {city: 'bronx'});
+      return store.createRecord('cat', {
+        zoo: zoo.cid,
+        name: 'baggage'
+      }).get('zoo').then((record) => {
+        record.state.city.should.eql('bronx');
+        record.should.eql(zoo);
+      });
+    });
+
+    it("Strips the belongsTo relation if the record has not been persisted.", () => {
+      let zoo = store.createRecord('zoo', {city: 'berlin'})
+      let brambles = store.createRecord('cat', {
+        zoo: zoo.cid,
+        name: 'brambles'
+      });
+      brambles.saveData().should.eql({name: 'brambles'});
     });
 
   });
