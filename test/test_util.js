8d18b798dc7a19fb4bf52eba6547791708d3ea5c
David Yeung <quxbaz@gmail.com>
2016-03-06 02:30:29 -0500

    util.capitalize()

--- test/test_util.js
+++ test/test_util.js
@@ -103,4 +103,19 @@ describe("lib/util", () => {
     });
   });
 
+  describe("capitalize()", () => {
+    it("Does nothing when passed an empty string.", () => {
+      util.capitalize('').should.eql('');
+    });
+    it("Capitalizes a single letter.", () => {
+      util.capitalize('a').should.eql('A');
+    });
+    it("Capitalizes a string.", () => {
+      util.capitalize('foo').should.eql('Foo');
+    });
+    it("Capitalizes only the first character of a multi-word string..", () => {
+      util.capitalize('foo foo').should.eql('Foo foo');
+    });
+  });
+
 });
